0172e6a0083ddb14cd5d4024de4588743e4d9630
hido <924838091@qq.com>
2019-03-27 14:54:00 +0800

    111

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,57 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <a-button type="danger" @click="showInfo()" icon="download">点击提示</a-button>
+        <ul v-for="item in arr" :key="item.id">
+            <li>{{item.applyUserName}}</li>
+        </ul>
+        <router-view/>
     </div>
-    <router-view/>
-  </div>
 </template>
 
 <style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
     }
-  }
-}
+
+    #nav {
+        padding: 30px;
+
+        a {
+            font-weight: bold;
+            color: #2c3e50;
+
+            &.router-link-exact-active {
+                color: #42b983;
+            }
+        }
+    }
 </style>
+
+<script>
+    import {
+        get,
+    } from './service/fetch.js';
+
+    export default {
+        data: function () {
+            return {
+                arr: [],
+            }
+        },
+        methods: {
+            showInfo: function () {
+                get({
+                    pageIndex: 1,
+                    pageSize: 10,
+                }).then((res) => {
+                    const data = res.data.toObject();
+                    this.arr = data.list;
+                })
+            }
+        }
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,36 @@
 import Vue from 'vue'
-import App from './App.vue'
 import router from './router'
 import store from './store'
+import config from './config.js'
+import loadingService from './utils/loadingService'
+import http from './utils/http'
+import './extension'
+import {
+	Button,
+	message,
+	notification,
+	Modal,
+} from 'ant-design-vue'
+import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.prototype.$message = message;
+Vue.prototype.$notification = notification;
+Vue.prototype.$info = Modal.info;
+Vue.prototype.$success = Modal.success;
+Vue.prototype.$error = Modal.error;
+Vue.prototype.$warning = Modal.warning;
+Vue.prototype.$confirm = Modal.confirm;
+Vue.prototype.$config = config;
+Vue.prototype.$loadingService = loadingService;
+Vue.prototype.$http = http;
+
+Vue.use(Button);
+Vue.use(Modal);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+	router,
+	store,
+	render: h => h(App)
 }).$mount('#app')
